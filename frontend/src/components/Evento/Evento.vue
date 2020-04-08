437a2feae059d6281f51e7c6133467c54893c8c2
Jefferson <jefferson.marin@correo.usa.edu.co>
2020-04-08 17:39:45 -0500

    add list events funcionality

--- frontend/src/components/Evento/Evento.vue
+++ frontend/src/components/Evento/Evento.vue
@@ -1,27 +1,76 @@
-<template>
-      <div class="container">
+<template lang="html">
+  <div class="container">
     <div class="row">
       <div class="col text-left">
-       <h2>Bienvenido</h2>
-         <div class="content">
-    <p>token: {{token}}</p>
-  </div>
-    <p>username: {{username}}</p>
-        
+        <div>
+        <h2>Listado de Eventos</h2>
+        <b-button size="sm" :to="{name:'NewBook'}" variant="primary">
+        Nuevo Evento
+        </b-button>
+        </div>
+        <br>
+        <div class="col-md-12">
+          <b-table striped hover :items="eventos":fields="fields">
 
+              <template v-slot:cell(action)="data">
+              <b-button size="sm" variant="primary" :to="{name:'EditBook',params: {bookId: data.item.id}}">
+              Editar
+              </b-button>
+             <b-button size="sm" variant="danger" :to="{name:'DeleteBook',params: {bookId: data.item.id}}">
+              Eliminar
+            </b-button>
+           </template>
 
+          </b-table>
+
+        </div>
       </div>
     </div>
-    </div>
+  </div>
 </template>
 <script>
+
+import axios from "axios";
+
 export default {
   props: {
     token: String,
     username: String
-  }
-}
-</script>
-<style lang="css" scope>
+  },
+ data() {
+    return {
+      fields: [
+        { key: "nombre", label: "Nombre" },
+        { key: "hora_inicio", label: "Hora Inicio" },
+        { key: "hora_final", label: "Hora Final" },
+        { key: "descripcion", label: "Descripcion" },
+        { key: "fecha", label: "Fecha" },
+        { key: "lugar", label: "lugar" },
+        { key: "action", label: "" }
+      ],
+      eventos: []
+    };
+  },
+  methods: {
+    getEventos() {
+      const path = "http://localhost:8000/evento/";
+      axios
+        .get(path)
+        .then(response => {
+          this.eventos = response.data;
 
-</style>>
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
+
+  created() {
+    this.getEventos();
+  }
+};
+</script>
+
+<style lang="css" scope>
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,8 +8,6 @@ import DeleteBook from '@/components/Book/DeleteBook'
 import Login from '@/components/Login/Login'
 import NewUser from '@/components/Login/NewUser'
 import Evento from '@/components/Evento/Evento'
-import test1 from '@/components/Evento/test1'
-import test2 from '@/components/Evento/test2'
 Vue.use(Router)
 
 export default new Router({
@@ -54,17 +52,6 @@ export default new Router({
       name: 'Evento',
       component: Evento,
       props: true
-    },
-    {
-      path: '/Evento/test1',
-      name: 'test1',
-      component: test1,
-      props: true
-    },
-    {
-      path: '/Evento/test2',
-      name: 'test2',
-      component: test2
     }
 
 
